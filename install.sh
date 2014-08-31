da4aa02628308cb814b56a66ca3b37aaa40a5dd5
Martin Bjeldbak Madsen <martinbmadsen@gmail.com>
2014-08-31 16:42:17 -0700

    Automate more things in install script (install vundle plugins, set shell to zsh)

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
- 
-rt PATH="/usr/local/bin:$PATH"
+# Remove .git directory copied over from bootstrap.sh
+rm -rf .git/ 
+#rt PATH="/usr/local/bin:$PATH"
 
 # make sure system is up-to-date
 sudo softwareupdate --install --all
@@ -10,18 +11,25 @@ ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go/install)"
 # Install homebrew apps
 brew bundle Brewfile
 
-# Python env
-pip install livestreamer
-
-# Ruby env
-rbenv install 2.1.2
-
-# Install some gems
-gem install --no-ri --no-rdoc bundler 
-
 # Install homebrew-cask apps
 brew bundle Caskfile
 
-# Bre cleanup
-brew cleanup --force -s
-rm -rf $(brew --cache)
+# Install vundle plugins
+vim +PluginInstall +qall
+
+# Setup Python env
+pip install livestreamer
+
+# Setup Ruby env
+rbenv install 2.1.2
+rbenv global 2.1.2
+rbenv rehash
+
+# Install Ruby bundler gem 
+gem install --no-ri --no-rdoc bundler 
+
+# Set shell to zsh
+chsh -s /bin/zsh
+
+# Delete myself
+rm -- "$0"
