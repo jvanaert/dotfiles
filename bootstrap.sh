239726ff79a28f771e1e7f3d431edac21cf8922d
Martin Bjeldbak Madsen <martinbmadsen@gmail.com>
2015-02-03 17:25:09 -0800

    Copy GNU ls solarized dark theme over

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,7 +8,9 @@ function doIt() {
 	rsync --exclude ".DS_Store" --exclude "bootstrap.sh" \
 		--exclude "README.md" --exclude "init/" --exclude ".osx" \
     -avh --no-perms . ~;
-	#source ~/.bash_profile;
+
+  # Copy GNU ls solarized dark theme over
+  cp init/dircolors-solarized/dircolors.256dark ~/.solarized-dark.dircolors
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
